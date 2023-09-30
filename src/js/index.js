b395ee681f9cefd162b0ec559e205e9c4e0253ff
cloudwipe <roma.isaev.2000@gmail.com>
2023-09-30 19:18:34 +0300

    working

--- src/js/index.js
+++ src/js/index.js
@@ -10,35 +10,48 @@ const refs = {
 refs.form.addEventListener('submit', onFormSubmit);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-console.log(refs.gallery);
-
 let searchQuery = '';
+let currentPage = 1;
+let totalHits = 0;
 
 function onFormSubmit(event) {
   event.preventDefault();
   searchQuery = event.currentTarget.elements.searchQuery.value;
 
+  clearGalleryContainer();
+  currentPage = 1;
+  refs.loadMoreBtn.classList.remove('show-load-more');
+
   getImageCard(searchQuery)
     .then(result => {
-      refs.form.reset();
-      renderImageCard(result);
-      refs.loadMore.classList.add('show-load-more');
+      renderImageCard(result.hits);
+      refs.loadMoreBtn.classList.add('show-load-more');
     })
     .catch(error => console.log(error));
 }
 
 async function getImageCard(searchQuery) {
+  if (totalHits === 500) {
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+    return;
+  }
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=39745378-dd24554a2bcf90950765bc548&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`
+      `https://pixabay.com/api/?key=39745378-dd24554a2bcf90950765bc548&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=20`
     );
     if (response.data.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
       console.log(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     }
-    return response.data.hits;
+    totalHits += 20;
+    return response.data;
   } catch (error) {
     console.log(
       'Sorry, there are no images matching your search query. Please try again.'
@@ -46,7 +59,12 @@ async function getImageCard(searchQuery) {
   }
 }
 
-function onLoadMore() {}
+function onLoadMore() {
+  currentPage += 1;
+  getImageCard(searchQuery).then(result => {
+    renderImageCard(result.hits);
+  });
+}
 
 function renderImageCard(data) {
   const markup = data
@@ -73,4 +91,8 @@ function renderImageCard(data) {
     .join('');
 
   refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearGalleryContainer() {
+  refs.gallery.innerHTML = '';
 }
