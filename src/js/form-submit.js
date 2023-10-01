0a1b03967c27d9260bb5af5f6b6ea76a050fb01a
cloudwipe <roma.isaev.2000@gmail.com>
2023-10-01 18:19:23 +0300

    working

--- src/js/form-submit.js
+++ src/js/form-submit.js
@@ -1,31 +1,29 @@
-import { refs } from './refs';
-import { getImageCard } from './api-service';
-import { renderImageCard } from './markup';
+// import { refs } from './refs';
+// import { getImageCard } from './api-service';
+// import { renderImageCard } from './markup';
 
-refs.form.addEventListener('submit', onFormSubmit);
+// let searchQuery = '';
 
-let searchQuery = '';
+// export function onFormSubmit(event) {
+//   event.preventDefault();
+//   searchQuery = event.currentTarget.elements.searchQuery.value;
 
-export function onFormSubmit(event) {
-  event.preventDefault();
-  searchQuery = event.currentTarget.elements.searchQuery.value;
+//   if (searchQuery === '') {
+//     refs.loadMoreBtn.classList.remove('show-load-more');
+//     refs.gallery.innerHTML = '';
+//     return;
+//   }
 
-  if (searchQuery === '') {
-    refs.loadMoreBtn.classList.remove('show-load-more');
-    refs.gallery.innerHTML = '';
-    return;
-  }
+//   refs.gallery.innerHTML = '';
+//   const currentPage = 1;
+//   refs.loadMoreBtn.classList.remove('show-load-more');
 
-  refs.gallery.innerHTML = '';
-  currentPage = 1;
-  refs.loadMoreBtn.classList.remove('show-load-more');
-
-  getImageCard(searchQuery, currentPage)
-    .then(result => {
-      console.log(result);
-      const render = renderImageCard(result.hits);
-      refs.gallery.insertAdjacentHTML('beforeend', render);
-      refs.loadMoreBtn.classList.add('show-load-more');
-    })
-    .catch(error => console.log(error));
-}
+//   getImageCard(searchQuery, currentPage)
+//     .then(result => {
+//       console.log(result);
+//       const render = renderImageCard(result.hits);
+//       refs.gallery.insertAdjacentHTML('beforeend', render);
+//       refs.loadMoreBtn.classList.add('show-load-more');
+//     })
+//     .catch(error => console.log(error));
+// }
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,51 @@
-// import axios from 'axios';
-// import Notiflix from 'notiflix';
-// import { renderImageCard } from './markup';
-// import { getImageCard } from './api-service';
-// import { refs } from './refs';
-import { onFormSubmit } from './form-submit';
-import { onLoadMore } from './load-more';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import { renderImageCard } from './markup';
+import { getImageCard } from './api-service';
+import { refs } from './refs';
+// import { onFormSubmit } from './form-submit';
+// import { onLoadMore } from './load-more';
 
-onFormSubmit;
-onLoadMore;
+refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+let searchQuery = '';
+let currentPage = 1;
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  searchQuery = event.currentTarget.elements.searchQuery.value;
+
+  if (searchQuery === '') {
+    refs.loadMoreBtn.classList.remove('show-load-more');
+    refs.gallery.innerHTML = '';
+    return;
+  }
+
+  refs.gallery.innerHTML = '';
+  const currentPage = 1;
+  refs.loadMoreBtn.classList.remove('show-load-more');
+
+  getImageCard(searchQuery, currentPage)
+    .then(result => {
+      const render = renderImageCard(result.hits);
+      refs.gallery.insertAdjacentHTML('beforeend', render);
+      refs.loadMoreBtn.classList.add('show-load-more');
+    })
+    .catch(error => console.log(error));
+}
+
+function onLoadMore() {
+  currentPage += 1;
+  getImageCard(searchQuery, currentPage).then(result => {
+    const render = renderImageCard(result.hits);
+    refs.gallery.insertAdjacentHTML('beforeend', render);
+    if (currentPage > result.totalHits / 20) {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      refs.loadMoreBtn.classList.remove('show-load-more');
+      return;
+    }
+  });
+}
